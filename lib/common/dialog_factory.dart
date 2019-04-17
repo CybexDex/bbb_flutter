0a66c1f58b9cd1d17892f4377c2755d57bce3688
KevinLi <shawnli1104@gmail.com>
2019-04-17 20:04:12 +0800

    添加Dialog

--- lib/common/dialog_factory.dart
+++ lib/common/dialog_factory.dart
@@ -4,9 +4,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DialogFactory {
-
   static CupertinoAlertDialog logoutConfirmDialog(BuildContext context,
-      {String title, String content, String cancel, String confirm, VoidCallback onConfirmPressed}) {
+      {String title,
+      String content,
+      String cancel,
+      String confirm,
+      VoidCallback onConfirmPressed}) {
     return CupertinoAlertDialog(
       title: Text(title),
       content: Text(content),
@@ -19,33 +22,48 @@ class DialogFactory {
         ),
         CupertinoDialogAction(
             child: Text(confirm, style: StyleFactory.dialogButtonFontStyle),
-            onPressed: onConfirmPressed
-        )
+            onPressed: onConfirmPressed)
       ],
-
     );
   }
 
   static CupertinoAlertDialog sellOrderDialog(BuildContext context,
-  {String title, String contentText, String value, String errorText, String cancel, String confirm, VoidCallback onConfirmPressed}) {
+      {String title,
+      String contentText,
+      String value,
+      String errorText,
+      String cancel,
+      String confirm,
+      VoidCallback onConfirmPressed}) {
     return CupertinoAlertDialog(
       title: Text(title),
-      content:
-        Column(
-          children: <Widget>[
-            Text(contentText),
-            Text(value),
-            Material(child:
-            TextFormField(
-              decoration: InputDecoration(
-                fillColor: Palette.subTitleColor.withOpacity(0.1),
-                filled: true,
-                border: InputBorder.none,
-                hintText: "请输入密",
+      content: Column(
+        children: <Widget>[
+          Text(contentText),
+          Text(value),
+          Container(
+            child: Material(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(4.0),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.only(top: 9, bottom: 9, left: 10, right: 16),
+                    hintText: "请输入密码",
+                    labelText: null,
+                    fillColor: Palette.subTitleColor.withOpacity(0.1),
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(style: BorderStyle.none, width: 0)),
+                    errorText: "密码错误"),
               ),
-            )),
-          ],
-        ),
+            ),
+          ),
+        ],
+      ),
       actions: <Widget>[
         CupertinoDialogAction(
           onPressed: () {
